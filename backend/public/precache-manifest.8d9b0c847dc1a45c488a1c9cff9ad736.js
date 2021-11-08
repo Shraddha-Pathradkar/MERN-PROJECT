c4620cdf9194650928f53638fe5d5dc60808aeca
Shraddha-Pathradkar <68277487+Shraddha-Pathradkar@users.noreply.github.com>
2021-11-08 15:09:50 -0800

    mern

---
+++ backend/public/precache-manifest.8d9b0c847dc1a45c488a1c9cff9ad736.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9d34e78a505a0c861c97d37f43ac3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e5bf01d2a863d69b7f3",
+    "url": "/static/css/0.10eff1aa.chunk.css"
+  },
+  {
+    "revision": "5a5325f1f1e6519e2a2b",
+    "url": "/static/css/4.6d344725.chunk.css"
+  },
+  {
+    "revision": "4e6b7f2361ec8d327c6f",
+    "url": "/static/css/5.b957ddbe.chunk.css"
+  },
+  {
+    "revision": "86201058a63587495932",
+    "url": "/static/css/6.a60aebec.chunk.css"
+  },
+  {
+    "revision": "fe7f47fca867a9076cfb",
+    "url": "/static/css/7.d37dee14.chunk.css"
+  },
+  {
+    "revision": "f532e475ba020dbdf740",
+    "url": "/static/css/8.6b80cc0f.chunk.css"
+  },
+  {
+    "revision": "cc71209f24f79fb9cba4",
+    "url": "/static/css/main.65269f5a.chunk.css"
+  },
+  {
+    "revision": "5e5bf01d2a863d69b7f3",
+    "url": "/static/js/0.0ea690e2.chunk.js"
+  },
+  {
+    "revision": "400ea4260c2fc45327e9",
+    "url": "/static/js/3.9da0d73c.chunk.js"
+  },
+  {
+    "revision": "5a5325f1f1e6519e2a2b",
+    "url": "/static/js/4.0ac4f68a.chunk.js"
+  },
+  {
+    "revision": "4e6b7f2361ec8d327c6f",
+    "url": "/static/js/5.a3b7e237.chunk.js"
+  },
+  {
+    "revision": "86201058a63587495932",
+    "url": "/static/js/6.c6334e74.chunk.js"
+  },
+  {
+    "revision": "fe7f47fca867a9076cfb",
+    "url": "/static/js/7.9510f5e6.chunk.js"
+  },
+  {
+    "revision": "f532e475ba020dbdf740",
+    "url": "/static/js/8.af543d45.chunk.js"
+  },
+  {
+    "revision": "cc71209f24f79fb9cba4",
+    "url": "/static/js/main.f846c336.chunk.js"
+  },
+  {
+    "revision": "bfd5d5cc78091a266631",
+    "url": "/static/js/runtime-main.5cef00c4.js"
+  }
+]);
